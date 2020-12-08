5f2f6afcfd4729754fcf76fa3b1489a08ad59348
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-12-08 17:31:17 +0200

    Add files via upload

---
+++ Neo4jDal/Interface/IFollowersDal.cs
@@ -0,0 +1,23 @@
+﻿using Neo4jDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neo4jDal.Interface
+{
+    public interface IFollowersDal
+    {
+        void AddUser(UserLableDTO u);
+        void DeleteUser(UserLableDTO u);
+        UserLableDTO GetUserById(int id);
+        void DeleteAllRelationships(UserLableDTO u1, UserLableDTO u2);
+        void AddFollower(UserLableDTO from, UserLableDTO to);
+        void AddFollower(int from, int to);
+        bool HaveAnyRelationship(UserLableDTO u1, UserLableDTO u2);
+        bool HaveRelationship(int from, int to);
+        int MinPathBetween(UserLableDTO u1, UserLableDTO u2);
+        int MinPathBetween(int id1, int id2);
+    }
+}
