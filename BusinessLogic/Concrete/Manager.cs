716476703ecbac9cf66e510772403f9e6c1fda7a
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-12-08 17:28:14 +0200

    Add files via upload

---
+++ BusinessLogic/Concrete/Manager.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLogic.Interface;
+using MongoDal.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Concrete
+{
+    public class Manager : IManager
+    {
+        private readonly IUserDal _mongoDal;
+        public Manager(IUserDal mongoDal)
+        {
+            this._mongoDal = mongoDal;
+        }
+
+        public bool Login(string Login, string Password)
+        {
+            try
+            {
+                return this._mongoDal.Login(Login, Password);
+            }
+            catch(Exception exp)
+            {
+                return false;
+            }
+        }
+    }
+}
